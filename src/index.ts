6cf5de03b6de9989df353a125692314e04503bde
Chetan Joshi <cj@Chetans-MacBook-Air.local>
2026-03-10 13:30:00 +0530

    testing the CI by pushing new error api

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 const PORT = 8080;
 
 app.get("/", (req, res) => {
-  res.send("This is Home Page");
-})
+	res.send("This is Home Page");
+});
 
 app.get("/testing", (req, res) => {
-  res.send("This is testing Page");
-})
+	res.send("This is testing Page");
+});
 
 app.get("/health", (req, res) => {
-  res.send("This is health Page");
-})
+	res.send("This is health Page");
+});
+
+app.get("/error", (req, res) => {
+	res.status(500).send("This is an error");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-})
+	console.log(`Server is listening on port ${PORT}`);
+});
